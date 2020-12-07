cd6c368eead7b9f55b2bccc0e0fc454f0c7ebabe
Jeremy <Jeremy@cates.org>
2020-12-07 17:07:06 -0500

    Jebsee Charge ability
    ability

---
+++ Symbiosis2/Assets/Scripts/Player/Ability_Charge.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Ability_Charge : MonoBehaviour
+{
+    public Transform jebseeOrientation;
+    public Rigidbody rigidBod;
+
+    private float chargeSpeed = 200.0f;
+    private float chargeTime = 0.5f;
+
+    private float currTime = 0.0f;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift) && currTime <= 0.0f)
+        {
+            currTime = chargeTime;
+        }
+
+        if(currTime > 0.0f)
+        {
+            currTime -= Time.deltaTime;
+
+            rigidBod.velocity = (-1*jebseeOrientation.forward) * chargeSpeed;
+        }
+    }
+}

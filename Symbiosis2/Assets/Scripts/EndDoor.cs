51471917a1b5eb9b82b7c954aeb899bd6e44445f
Alex <alexjortiz7430@gmail.com>
2020-12-06 21:59:32 -0700

    Updated Grapple action block and started on win condition

---
+++ Symbiosis2/Assets/Scripts/EndDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndDoor : MonoBehaviour
+{
+    public GameObject winPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        winPanel.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+}

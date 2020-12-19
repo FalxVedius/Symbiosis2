0f7a2f07ce556f021978c0efb1121561c62477d1
Alex <alexjortiz7430@gmail.com>
2020-12-18 17:01:00 -0700

    Updated Tandem Action block with more art assets

---
+++ Symbiosis2/Assets/Scripts/UI/AudioSlider.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSlider : MonoBehaviour
+{
+    //private variables
+    AudioManager audioManager;
+    Slider slider;
+
+    public void Awake()
+    {
+        slider = this.gameObject.GetComponent<Slider>(); //Assigns slider variable to gameobject's slider component
+        if (slider == null)
+        {
+            Debug.LogError("Slider not assigned");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Assigns audioManager variable to AudioManager
+        audioManager = AudioManager.instance;
+
+
+        if (audioManager == null) //Checks if the Audio Manager was properly assigned
+        {
+            Debug.Log("Audio Manager not assigned");
+        }
+        else
+        {
+            //If Audio Manager is properly assigned, check what type of volume the slider is adjusting and assign its value accordingly
+            if (this.gameObject.name == "BackgroundSlider")
+            {
+                slider.value = audioManager.GetMusicVolume();
+
+            }
+            else if (this.gameObject.name == "SFXSlider")
+            {
+                slider.value = audioManager.GetSFXVolume();
+            }
+            else
+            {
+                Debug.Log(gameObject.name + " doesn't have proper slider name on it");
+            }
+        }
+    }
+
+    public void SetMasterVolume(float masterVol)
+    {
+        //Sets Master Volume Level
+        audioManager.SetMasterVolume(masterVol);
+    }
+
+    public void SetMusicVolume(float musicVol)
+    {
+        //Sets Music Volume Level
+        audioManager.SetMusicVolume(musicVol);
+    }
+
+    public void SetSFXVolume(float sfxVol)
+    {
+        //Sets SFX Volume Level
+        audioManager.SetSFXVolume(sfxVol);
+    }
+
+    public void PlayTestSound()
+    {
+        audioManager.PlaySound("TestSound");
+    }
+}

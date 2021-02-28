4fde3c12848e043d0f591812c4d0429e8f24232e
Jake Wade <wadejakej@gmail.com>
2021-02-28 14:45:17 -0500

    Levels flow all the way through, added more final models to controlroom

--- Symbiosis2/Assets/Scripts/EndDoor.cs
+++ Symbiosis2/Assets/Scripts/EndDoor.cs
@@ -8,18 +8,6 @@ public class EndDoor : MonoBehaviour
     public GameObject winPanel;
     public bool finalLevel;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         if (finalLevel)
